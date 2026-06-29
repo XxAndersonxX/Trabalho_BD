1abb0b9969377205a2d246703a3c75917a5441c6
Daniel Alves <danivelsantana@gmail.com>
2026-06-29 11:21:53 -0300

    foi acrescentado o tipo de dado retornado pelos métodos

--- soeu/src/main/java/com/soeu/dao/factory/DaoFactory.java
+++ soeu/src/main/java/com/soeu/dao/factory/DaoFactory.java
@@ -11,46 +11,56 @@ import com.soeu.dao.impl.ProvaDAO;
 import com.soeu.dao.impl.TarefaDAO;
 import com.soeu.dao.impl.TurmaDAO;
 import com.soeu.dao.interfaces.EntityDAO;
+import com.soeu.entities.Aluno;
+import com.soeu.entities.Disciplina;
+import com.soeu.entities.GrupoEstudo;
+import com.soeu.entities.HorarioEstudo;
+import com.soeu.entities.MetasEstudo;
+import com.soeu.entities.Notificacoes;
+import com.soeu.entities.Professor;
+import com.soeu.entities.Prova;
+import com.soeu.entities.Tarefa;
+import com.soeu.entities.Turma;
 import com.soeu.util.DB;
 
 public class DaoFactory {
-    public static EntityDAO createAlunoDao(){
+    public static EntityDAO<Aluno> createAlunoDao(){
         return new AlunoDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createDisciplinaDAO(){
+    public static EntityDAO<Disciplina> createDisciplinaDAO(){
         return new DisciplinaDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createGrupoEstudo(){
+    public static EntityDAO<GrupoEstudo> createGrupoEstudo(){
         return new GrupoEstudoDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createHorarioEstudoDAO(){
+    public static EntityDAO<HorarioEstudo> createHorarioEstudoDAO(){
         return new HorarioEstudoDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createMetasEstudoDAO(){
+    public static EntityDAO<MetasEstudo> createMetasEstudoDAO(){
         return new MetasEstudoDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createNotificacoesDAO(){
+    public static EntityDAO<Notificacoes> createNotificacoesDAO(){
         return new NotificacoesDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createProfessorDAO(){
+    public static EntityDAO<Professor> createProfessorDAO(){
         return new ProfessorDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createProvaDAO(){
+    public static EntityDAO<Prova> createProvaDAO(){
         return new ProvaDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createTarefaDAO(){
+    public static EntityDAO<Tarefa> createTarefaDAO(){
         return new TarefaDAO(DB.getMySQLConnection());
     }
 
-    public static EntityDAO createTurmaDAO(){
+    public static EntityDAO<Turma> createTurmaDAO(){
         return new TurmaDAO(DB.getMySQLConnection());
     }
 }
