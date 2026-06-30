265c69b8702d3bd454ddbfa8edbdf8ab8acb1f47
Daniel Alves <danivelsantana@gmail.com>
2026-06-30 09:27:36 -0300

    aqui ficará o fluxo de telas

--- soeu/src/main/java/com/soeu/App.java
+++ soeu/src/main/java/com/soeu/App.java
@@ -1,16 +1,12 @@
 package com.soeu;
 
-import java.sql.Connection;
-
-import com.soeu.conexaobanco.conexaofac;
+import com.soeu.view.LoginTela;
 
 public class App {
 
     public static void main(String[] args) {
-        Connection conexao = conexaofac.getConnection();
-
-        if (conexao != null) {
-            System.out.println("Conectado com sucesso!");
-        }
+         java.awt.EventQueue.invokeLater(() -> {
+            new LoginTela().setVisible(true);
+        });
     }
 }
