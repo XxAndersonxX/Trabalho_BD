c78ad22e28683629be362477881bf4baa6c75971
Daniel Alves <danivelsantana@gmail.com>
2026-07-03 14:41:21 -0300

    fazer teste na tela de Grupos

--- soeu/src/main/java/com/soeu/App.java
+++ soeu/src/main/java/com/soeu/App.java
@@ -1,12 +1,18 @@
 package com.soeu;
 
-import com.soeu.view.LoginTela;
+import com.soeu.entities.GrupoEstudo;
+import com.soeu.view.GrupoEstudoTela;
 
 public class App {
 
     public static void main(String[] args) {
-         java.awt.EventQueue.invokeLater(() -> {
-            new LoginTela().setVisible(true);
+        java.awt.EventQueue.invokeLater(() -> {
+            GrupoEstudo grupo = new GrupoEstudo();
+
+            grupo.setIdGrupo(1);
+            grupo.setDescricao("Programação Java");
+
+            new GrupoEstudoTela(grupo).setVisible(true);
         });
     }
 }
