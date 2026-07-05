16ca2bbbbd090a110ee0931087533e13537c5820
Daniel Alves <danivelsantana@gmail.com>
2026-07-05 13:05:56 -0300

    a aplicação começa pela tela de login

--- soeu/src/main/java/com/soeu/App.java
+++ soeu/src/main/java/com/soeu/App.java
@@ -1,14 +1,10 @@
 package com.soeu;
 
-//import com.soeu.entities.GrupoEstudo;
-//import com.soeu.view.GrupoEstudoTela;
 import com.soeu.view.LoginTela;
 
 public class App {
 
-        public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new LoginTela().setVisible(true);
-        });
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new LoginTela().setVisible(true));
     }
 }
